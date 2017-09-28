673c394035227c5f5338fb56dcf25c1dba25a901
Ximo Guanter Gonzálbez <joaquin.guantergonzalbez@telefonica.com>
2017-09-28 13:28:33 +0200

    stuff for load testing

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
-var loggerConfig = require('./configs/morgan.config');
-var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// var loggerConfig = require('./configs/morgan.config');
+// var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var app = express();
 
-loggerConfig.setup(logger);
-app.use(logger(loggerConfig.log));
+// loggerConfig.setup(logger);
+// app.use(logger(loggerConfig.log));
 
-const errorsRoutes = require('./routes/errors.route');
+// const errorsRoutes = require('./routes/errors.route');
 
 app.use(bodyParser.json());
-app.use(bodyParser.text());
-app.use(bodyParser.raw());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(bodyParser.text());
+// app.use(bodyParser.raw());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
 
 app.use(function (req, res, next) {
+  const current = Date.now().valueOf();
+  const code = req.params.code;
+  const sent = Number(req.body.payload.sent) * 1000;
+  console.log(current - sent);
   res.status(200).send();
 });
 
-app.use('/errors', errorsRoutes);
+// app.use('/errors', errorsRoutes);
 
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json(err);
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.json(err);
+// });
 
 module.exports = app;
--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -1,5 +1,4 @@
 
 module.exports.coded = (req, res, next) => {
-    let code = req.params.code;
     res.status(code).send();
 }
